a954256eff1f82f4be11461139ab3ae26fccfc2d
Ekaterina <kotofeya@yandex.ru>
2020-11-20 21:43:42 +0300

    activity_main_details

---
+++ app/src/main/java/com/abramovae/newproject/MovieDetailsActivity.kt
@@ -0,0 +1,14 @@
+package com.abramovae.newproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+
+
+}
