6a600ab9cda2680e672446a89c6929ecce6bade5
Ekaterina <kotofeya@yandex.ru>
2020-11-29 20:43:59 +0300

    homework2 additional

--- app/src/main/java/com/abramovae/newproject/FragmentMoviesDetails.kt
+++ app/src/main/java/com/abramovae/newproject/FragmentMoviesDetails.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
 class FragmentMoviesDetails: Fragment(){
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
